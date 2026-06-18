58939dc28acccbf4feaed7a1cf9a827c9eb3d521
Richmond Amoateng | Level 200 Electrical/Electronics Eng. Student <richmondamoateng0@gmail.com>
2026-06-18 10:06:22 +0000

    Add files via upload

---
+++ 3. Equality Check for Three Integers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, z;
+    cout << "Enter three integers: ";
+    cin >> x >> y >> z;
+
+    if (x == y && y == z)
+        cout << "Equal" << endl;
+    else
+        cout << "Not equal" << endl;
+
+    return 0;
+}
